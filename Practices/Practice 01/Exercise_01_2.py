5e59bdda1922eaa2d4a765dc77eb87ce0623a393
sharon1160 <schullunquia@unsa.edu.pe>
2020-09-15 19:10:05 -0500

    File with random numbers in python

---
+++ Practices/Practice 01/Exercise_01_2.py
@@ -0,0 +1,10 @@
+import random
+
+###### MAIN ######
+
+quantity = 1000000
+vector = []
+# vector = [random.randint(1, quantity) for x in range(quantity)] # Numbers within of the range 0f 1 to 1000000
+vector = [random.randint(1, 10) for x in range(quantity)] # Numbers within of the range 0f 1 to 10
+for x in vector:
+    print(x)
