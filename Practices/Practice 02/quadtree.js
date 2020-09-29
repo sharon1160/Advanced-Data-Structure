1fd0654809b911af14692d2da898e70db8c9591c
sharon1160 <schullunquia@unsa.edu.pe>
2020-09-29 16:44:08 -0500

    Creating quadtree.js file

---
+++ Practices/Practice 02/quadtree.js
@@ -0,0 +1,23 @@
+class Point {
+    constructor (x, y, userData ){
+    this .x = x;
+    this .y = y;
+    this . userData = userData ;
+    }
+}
+class Rectangle {
+    constructor (x, y, w, h){
+        this .x = x; // center
+        this .y = y;
+        this .w = w; // half width
+        this .h = h; // half height
+    }
+    // verifica si este objeto contiene un objeto Punto
+    contains ( point ){
+
+    }
+    // verifica si este objeto se intersecta con otro objeto Rectangle
+    intersects ( range ){
+        
+    }
+}
